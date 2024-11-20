81119c9e2686c2873b75efd4f7a2a88759919cf9
Joelle-arch <jsn34@njit.edu>
2024-11-19 23:42:50 -0500

    update

--- bootstrap/4thpage.js
+++ bootstrap/4thpage.js
@@ -1,1 +1,14 @@
-let 
+let month = 10;
+let day = 12;
+
+if (month < 10){
+console.log("Remind me before event");
+}
+
+else if (month = 10 && day<=12){
+console.log("Remind me before event");
+}
+
+else{
+console.log("Reminder invalid");
+}
